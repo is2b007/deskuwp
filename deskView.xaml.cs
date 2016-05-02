88f11f549e54d55ec42562a85f175fd551e199db
is2b007 <is2b007@gmail.com>
2016-05-02 14:06:33 +0930

    DeskX Initial
    Initial repo added

---
+++ deskView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace deskuwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class deskView : Page
+    {
+        public deskView()
+        {
+            this.InitializeComponent();
+
+            inkCanvas.InkPresenter.InputDeviceTypes =
+            Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+            Windows.UI.Core.CoreInputDeviceTypes.Pen |
+            Windows.UI.Core.CoreInputDeviceTypes.Touch;
+
+            // Set initial ink stroke attributes.
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Black;
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+        }
+        private void OnPenColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                InkDrawingAttributes drawingAttributes =
+                    inkCanvas.InkPresenter.CopyDefaultDrawingAttributes();
+
+                string value = ((ComboBoxItem)PenColor.SelectedItem).Content.ToString();
+
+                switch (value)
+                {
+                    case "Black":
+                        drawingAttributes.Color = Windows.UI.Colors.Black;
+                        break;
+                    case "Red":
+                        drawingAttributes.Color = Windows.UI.Colors.Red;
+                        break;
+                    default:
+                        drawingAttributes.Color = Windows.UI.Colors.Black;
+                        break;
+                };
+
+                inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            }
+        }
+    }
+}
